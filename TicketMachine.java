84a49cb0b3af178641057bf674014878fa2cb35a
ZipNobles <chris@zipcodewilmington.com>
2019-10-08 14:22:24 -0400

    Lots of modifications. Got rid of sysin and sysout necessity. Still need to edit README. Added tests. This needs a look to improve testing, likely improve flow of lab overall.

--- TicketMachine.java
+++ TicketMachine.java
@@ -12,37 +12,49 @@
 public class TicketMachine
 {
     // The price of a ticket from this machine.
-    private int price;
+    private Integer price;
     // The amount of money entered by a customer so far.
-    private int balance;
+    private Integer balance;
     // The total amount of money collected by this machine.
-    private int total;
+    private Integer total;
+    // The number of tickets printed.
+    private Integer ticketNumber;
 
     /**
      * Create a machine that issues tickets of the given price.
      * Note that the price must be greater than zero, and there
      * are no checks to ensure this.
      */
-    public TicketMachine(int ticketCost)
+    public TicketMachine(Integer ticketCost)
     {
         price = ticketCost;
         balance = 0;
         total = 0;
+        ticketNumber = 0;
     }
 
     /**
      * Return the price of a ticket.
      */
-    public int getPrice()
+    public Integer getPrice()
     {
         return price;
+    }
+    
+    /**
+     * Return ticketNumber.
+     * (Increments on each print.)
+     */
+    public Integer getTicketNumber()
+    {
+        return ticketNumber;
     }
 
     /**
      * Return the amount of money already inserted for the
      * next ticket.
      */
-    public int getBalance()
+    public Integer getBalance()
     {
         return balance;
     }
@@ -50,29 +62,37 @@ public class TicketMachine
     /**
      * Receive an amount of money in cents from a customer.
      */
-    public void insertMoney(int amount)
+    public Integer insertMoney(Integer amount)
     {
         balance = balance + amount;
+        return balance;
     }
+    
+    public Integer calculateTotal(){
+        total = balance + total;
+        return total;
+    }
+    
+    public Integer incrementTicketNumber(){
+        ticketNumber++;
+        return ticketNumber;
+    }
+    
 
     /**
      * Print a ticket.
      * Update the total collected and
      * reduce the balance to zero.
      */
-    public void printTicket()
+    public String printTicket()
     {
-        // Simulate the printing of a ticket.
-        System.out.println("##################");
-        System.out.println("# The BlueJ Line");
-        System.out.println("# Ticket");
-        System.out.println("# " + price + " cents.");
-        System.out.println("##################");
-        System.out.println();
-
+        //Increment the number of tickets printed
+        incrementTicketNumber();
         // Update the total collected with the balance.
         total = total + balance;
         // Clear the balance.
         balance = 0;
+        
+        return "Ticket price: " + price + " cents. " + "Your total is " + total + ".";
     }
 }
---
+++ TicketMachineTest.java
@@ -0,0 +1,67 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class TicketMachineTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class TicketMachineTest
+{
+    /**
+     * Default constructor for test class TicketMachineTest
+     */
+    public TicketMachineTest()
+    {
+    }
+    
+    @Before
+    public void setUp()
+    {
+    }
+    
+    @After
+    public void tearDown()
+    {
+    }
+    
+    @Test
+    public void insertMoneyTest(){
+        TicketMachine testTicketMachine = new TicketMachine(4);
+        
+        testTicketMachine.insertMoney(20);
+        
+        assertEquals(Integer.valueOf(20), testTicketMachine.getBalance());     
+    }
+    
+    @Test
+    public void printTicketTest(){
+        TicketMachine testTicketMachine = new TicketMachine(13);
+        
+        assertEquals("Ticket price: 13 cents. Your total is 0.",testTicketMachine.printTicket()); 
+    }
+    
+    @Test
+    public void printTicketTest2(){
+        TicketMachine testTicketMachine = new TicketMachine(40);
+        
+        assertNotEquals("Ticket price: 13 cents. Your total is 0.", testTicketMachine.printTicket());        
+    }
+    
+    @Test
+    public void incrementTicketNumberTest(){
+        TicketMachine testTicketMachine = new TicketMachine(40);
+        
+        for(int i = 0; i < 10; i++){
+            testTicketMachine.printTicket();
+    }
+    
+        assertEquals(Integer.valueOf(10), testTicketMachine.getTicketNumber()); 
+    }
+    
+}
